eaa6a4d2b2bd618b2b8d743f64cff10fcf2d7d4d
M0ose <m0ose@yahoo.com>
2015-07-16 09:49:20 -0600

    fill holes

--- week1/triangle2.js
+++ week1/triangle2.js
@@ -11,6 +11,7 @@ GLtriangle = {
     
     theta : 0.0, // initial angle for spin
     sierpinskiateIterations : 4, // the triangle count is 3^n and can get big fast
+    fillMesh:false,
     initialVertices : [
             vec3(  0.0,  1.0 , 0 ),
             vec3( -Math.cos(Math.PI/6),-Math.sin(Math.PI/6), 0 ),
@@ -62,18 +63,20 @@ GLtriangle = {
     //tesselate the triangle. Also tesselate the colors just for fun.
     //
     updateVertices: function( iterations) {
+        if( iterations === undefined) { iterations = this.sierpinskiateIterations }
         this.sierpinskiateIterations = Number(iterations) //|| this.sierpinskiateIterations
-        this.vertices = this.sierpinskiate(this.initialVertices, this.sierpinskiateIterations)
-        this.colors = this.sierpinskiate(this.initialColors, this.sierpinskiateIterations)
+        this.vertices = this.sierpinskiate(this.initialVertices, this.sierpinskiateIterations, this.fillMesh)
+        this.colors = this.sierpinskiate(this.initialColors, this.sierpinskiateIterations, this.fillMesh)
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer)
         gl.bufferData( gl.ARRAY_BUFFER, flatten(this.vertices), gl.STATIC_DRAW)
         gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer)
         gl.bufferData( gl.ARRAY_BUFFER, flatten(this.colors), gl.STATIC_DRAW)
+        console.log("# of vertices", this.vertices.length)
     },
     //
     // turn a normal triangle into a serpinkskis gasket
     //  
-    sierpinskiate: function(vertices, iterations){
+    sierpinskiate: function(vertices, iterations, fillMesh){
         //console.log(iterations)
         if(iterations <= 0) {
             return vertices
@@ -92,13 +95,16 @@ GLtriangle = {
             tmpVerts.push([v1,u1,u3])
             tmpVerts.push([u1,v2,u2])
             tmpVerts.push([u3,u2,v3])
+            if(fillMesh) {
+                tmpVerts.push([u1,u2,u3])
+            }
         }
         for(var i=0; i < tmpVerts.length; i++) {
             vertOut.push( tmpVerts[i][0])
             vertOut.push( tmpVerts[i][1])
             vertOut.push( tmpVerts[i][2])
         }
-        return this.sierpinskiate(vertOut, iterations-1)
+        return this.sierpinskiate(vertOut, iterations-1, fillMesh)
     },
 
     updateTheta: function( theta) {
