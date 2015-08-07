c1b8fb5d49ff33a43653d555a5fe45646e13b792
M0ose <m0ose@yahoo.com>
2015-08-07 15:57:02 -0600

    sobel filter test

---
+++ weekX/sobel.js
@@ -0,0 +1,92 @@
+
+function glSobel() {
+
+    var texCoord = [
+        vec2(0, 0),
+        vec2(0, 1),
+        vec2(1, 1),
+        vec2(1, 1),
+        vec2(1, 0),
+        vec2(0, 0)
+    ];
+
+    var vertices = [
+        vec2( -1, -1 ),
+        vec2(  -1,  1 ),
+        vec2(  1, 1 ),
+        vec2( 1, 1 ),
+        vec2(  1,  -1 ),
+        vec2(  -1, -1 )
+    ];
+
+    this.init = function() {
+        //setup webgl
+        this.canvas = document.getElementById( "gl-canvas" )
+        gl = WebGLUtils.setupWebGL( this.canvas )
+        if ( !gl ) { throw( "WebGL isn't available" ) }
+        // setup view
+        this.changeViewPort(this.canvas.width, this.canvas.height)
+        gl.clearColor(0.0, 0.0, 0.0, 1.0)
+        // Create an empty textures
+        this.texture1 = gl.createTexture()
+        gl.activeTexture( gl.TEXTURE0 )
+        //
+        this.programSobel = initShadersFromStrings( gl, sobelShaders.vertex, sobelShaders.fragment )
+        // vertices
+        gl.useProgram(this.programSobel)
+        this.bufferV = gl.createBuffer()
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.bufferV)
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW)
+        var vPosition = gl.getAttribLocation( this.programSobel, "vPosition" )
+        gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 )
+        gl.enableVertexAttribArray( vPosition )
+        // texture cooridinates
+        this.bufferTC = gl.createBuffer()
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.bufferTC)
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoord), gl.STATIC_DRAW)
+        var vTexCoord = gl.getAttribLocation( this.programSobel, "vTexCoord") 
+        gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 )
+        gl.enableVertexAttribArray( vTexCoord )
+        //
+        gl.uniform1i( gl.getUniformLocation(this.programSobel, "texture"), 0)
+    }
+
+    this.changeImage = function(image) {
+        this.changeViewPort(image.width, image.height)
+        this.configTexture( this.texture1, image)
+        this.image = image
+    }
+
+    this.changeViewPort = function(wid, hei){
+        if( wid !== this.width || hei !== this.height) {
+            this.width = wid
+            this.height = hei
+            this.canvas.width = wid
+            this.canvas.height = hei
+            gl.viewport(0, 0, wid, hei)
+            console.log("viewport dimensions changed to ", wid, hei)
+        }
+    }
+
+    this.render = function() {
+        gl.bindFramebuffer( gl.FRAMEBUFFER, null)
+        gl.clear( gl.COLOR_BUFFER_BIT )
+        gl.drawArrays(gl.TRIANGLES, 0, 6)
+    }
+
+    this.configTexture = function( tex, image) {
+        var td = gl.getUniformLocation(this.programSobel, 'texDimensions')
+        gl.uniform2fv(td, [image.width, image.height])
+        gl.bindTexture(gl.TEXTURE_2D, tex);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    }
+
+    this.init()
+}
+
+
+
---
+++ weekX/sobelShaders.js
@@ -0,0 +1,51 @@
+
+var sobelShaders = {
+
+    vertex : `
+        precision mediump float;
+        attribute vec4 vPosition;
+        attribute vec2 vTexCoord;
+        uniform vec2 texDimensions;
+        varying vec2 texDims;
+        varying vec2 fTexCoord;
+
+        void main() {
+            fTexCoord = vTexCoord;
+            texDims = texDimensions;
+            gl_Position = vPosition;
+        }
+    `,
+
+    fragment : `
+        precision mediump float;
+        uniform sampler2D texture;
+        varying vec2 fTexCoord;
+        varying vec2 texDims;
+
+        float lum(vec4 c) {
+            return dot(c.rgb, vec3(0.3, 0.59, 0.11));
+        }
+
+
+        void main() {
+            vec2 p = fTexCoord.xy ;
+            vec2 dp = vec2(1.0, 1.0) / texDims;
+            vec4 tcolor = texture2D( texture, fTexCoord.xy);
+            // find gradient in image
+            float t00 = lum(texture2D(texture, p + dp * vec2(-1.0, -1.0)));
+            float t10 = lum(texture2D(texture, p + dp * vec2( 0.0, -1.0)));
+            float t20 = lum(texture2D(texture, p + dp * vec2( 1.0, -1.0)));
+            float t01 = lum(texture2D(texture, p + dp * vec2(-1.0,  0.0)));
+            float t21 = lum(texture2D(texture, p + dp * vec2( 1.0,  0.0)));
+            float t02 = lum(texture2D(texture, p + dp * vec2(-1.0,  1.0)));
+            float t12 = lum(texture2D(texture, p + dp * vec2( 0.0,  1.0)));
+            float t22 = lum(texture2D(texture, p + dp * vec2( 1.0,  1.0)));
+            vec2 grad;
+            grad.x = t00 + 2.0 * t01 + t02 - t20 - 2.0 * t21 - t22;
+            grad.y = t00 + 2.0 * t10 + t20 - t02 - 2.0 * t12 - t22;
+
+            gl_FragColor = vec4(grad,0.0,1.0);//tcolor + vec4(grad/3.0, 0.0, 0.0);
+        }
+    `,
+
+}
